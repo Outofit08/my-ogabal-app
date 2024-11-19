946595e4721687c8ea4e3db0837e6fa9e653014b
Brashad <bhasley08@gmail.com>
2024-11-18 18:14:13 -0700

    quick start

---
+++ src/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Add your scripts here
+</script>
+
+<svelte:head>
+  <title>Welcome to ogabal.com</title>
+</svelte:head>
+
+<h1>Welcome to ogabal.com</h1>
+
+<style>
+  h1 {
+    color: #333;
+    font-size: 2.5rem;
+  }
+</style> 
